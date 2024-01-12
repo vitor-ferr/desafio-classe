41f811439b50bb0d3137525f6dc1e393df5ef42d
vitor-ferr <vitolemos55@gmail.com>
2024-01-12 16:12:38 -0300

    desafio classe

---
+++ classeheroi.js
@@ -0,0 +1,16 @@
+let heroi = {
+    nome: "Vitor",
+    idade: 22,
+    tipo: "Ninja",
+};
+
+if (heroi.tipo === "Guerreiro") {
+    console.log("O " + heroi.tipo + " atacou usando Espada!");
+} else if (heroi.tipo === "Mago") {
+	console.log ("O " + heroi.tipo + " atacou usando Magia!");
+} else if (heroi.tipo === "Monge") {
+	console.log("O " + heroi.tipo + " atacou usando Artes marciais!");
+} else if (heroi.tipo === "Ninja") {
+	console.log("O " + heroi.tipo + " atacou usando Shuriken!")
+}
+	
